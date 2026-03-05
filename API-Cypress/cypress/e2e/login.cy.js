028d2c4072df89eb80075fa5d14e448d42790a11
vasc1086 <74975016+vasc1086@users.noreply.github.com>
2026-03-05 08:10:37 -0500

    Update login.cy.js

--- API-Cypress/cypress/e2e/login.cy.js
+++ API-Cypress/cypress/e2e/login.cy.js
@@ -1,1 +1,24 @@
-// Login API test
+
+describe('API Login Tests', () => {
+
+  it('Login exitoso', () => {
+    cy.request('POST', 'https://api.demoblaze.com/login', {
+      username: 'usuarioNuevo123',
+      password: 'pass123'
+    }).then((resp) => {
+      expect(resp.status).to.eq(200);
+    });
+  });
+
+  it('Login incorrecto', () => {
+    cy.request({
+      method: 'POST',
+      url: 'https://api.demoblaze.com/login',
+      body: { username: 'mal', password: 'incorrecto' },
+      failOnStatusCode: false
+    }).then((resp) => {
+      expect(resp.status).to.be.oneOf([200, 400]);
+    });
+  });
+
+});
