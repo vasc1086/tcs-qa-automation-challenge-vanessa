aadce10038374b21e035d2878c8037f4c39c7bcb
vasc1086 <74975016+vasc1086@users.noreply.github.com>
2026-03-05 08:10:16 -0500

    Add signup API tests for new and existing users

--- API-Cypress/cypress/e2e/signup.cy.js
+++ API-Cypress/cypress/e2e/signup.cy.js
@@ -1,1 +1,31 @@
-// Signup API test
+
+describe('API Signup Tests', () => {
+
+  it('Crear usuario nuevo', () => {
+    cy.fixture('usuarios').then(users => {
+      cy.request('POST', 'https://api.demoblaze.com/signup', {
+        username: users[0].user,
+        password: users[0].password
+      }).then((resp) => {
+        expect(resp.status).to.eq(200);
+      });
+    });
+  });
+
+  it('Intentar crear usuario existente', () => {
+    cy.fixture('usuarios').then(users => {
+      cy.request({
+        method: 'POST',
+        url: 'https://api.demoblaze.com/signup',
+        body: {
+          username: users[1].user,
+          password: users[1].password
+        },
+        failOnStatusCode: false
+      }).then((resp) => {
+        expect(resp.status).to.be.oneOf([200, 400]);
+      });
+    });
+  });
+
+});
